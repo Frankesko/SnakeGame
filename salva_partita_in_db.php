0f2bc0f9c9859774676acff446877842e3df704b
Francesco Bartolomeo <francibartolomeo@libero.it>
2024-01-09 12:44:49 +0100

    aa prova

--- salva_partita_in_db.php
+++ salva_partita_in_db.php
@@ -3,37 +3,48 @@ $conn = require('db_conn.php');
 // Ricevi i dati inviati dal client (JavaScript)
 $data = json_decode(file_get_contents("php://input"));
 
-if (!$data) {
-    echo "Dati non validi o mancanti.";
-} else {
+// Verifica del metodo HTTP richiesto
+$method = $_SERVER['REQUEST_METHOD'];
 
-    $id_utente = $data->id_utente;
-    $score = $data->score;
-    $numero_cibo = $data->numero_cibo;
-    $dimensione_serpente = $data->dimensione_serpente;
-    $speed = $data->speed;
-
-
-    $stmt = $conn->prepare("SELECT coins FROM utenti where id_utente = ?");
-    $stmt->execute([$id_utente]);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    $oldCoinsAmount = $row["coins"];
-
-    $newCoinsAmount = $oldCoinsAmount + $score;
-
-    $stmt = $conn->prepare("UPDATE utenti SET coins = ? WHERE id_utente = ?");
-    $stmt->execute([$newCoinsAmount, $id_utente]);
-
-    // Esegui l'inserimento nel database
-    $stmt = $conn->prepare("INSERT INTO partite (id_utente, score, numero_cibo, dimensione_serpente, speed) VALUES (?, ?, ?, ?, ?)");
-    $stmt->execute([$id_utente, $score, $numero_cibo, $dimensione_serpente, $speed]);
-
-    // Verifica se l'inserimento è riuscito
-    if ($stmt->rowCount() > 0) {
-        echo "Inserimento avvenuto";
+// Se il metodo è POST, gestisci la richiesta
+if ($method === 'POST') {
+    if (!$data) {
+        echo "Dati non validi o mancanti.";
     } else {
-        echo "Errore durante inserimento.";
-    
+
+        $id_utente = $data->id_utente;
+        $score = $data->score;
+        $numero_cibo = $data->numero_cibo;
+        $dimensione_serpente = $data->dimensione_serpente;
+        $speed = $data->speed;
+
+
+        $stmt = $conn->prepare("SELECT coins FROM utenti where id_utente = ?");
+        $stmt->execute([$id_utente]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $oldCoinsAmount = $row["coins"];
+
+        $newCoinsAmount = $oldCoinsAmount + $score;
+
+        $stmt = $conn->prepare("UPDATE utenti SET coins = ? WHERE id_utente = ?");
+        $stmt->execute([$newCoinsAmount, $id_utente]);
+
+        // Esegui l'inserimento nel database
+        $stmt = $conn->prepare("INSERT INTO partite (id_utente, score, numero_cibo, dimensione_serpente, speed) VALUES (?, ?, ?, ?, ?)");
+        $stmt->execute([$id_utente, $score, $numero_cibo, $dimensione_serpente, $speed]);
+
+        // Verifica se l'inserimento è riuscito
+        if ($stmt->rowCount() > 0) {
+            echo "Inserimento avvenuto";
+        } else {
+            echo "Errore durante inserimento.";
+        
+        }
     }
+} else {
+    // Se il metodo non è consentito, restituisci un messaggio di errore
+    header('HTTP/1.1 405 Method Not Allowed');
+    echo json_encode(array('message' => 'Metodo non consentito'));
 }
+
 ?>
